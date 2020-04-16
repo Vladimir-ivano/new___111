44f6e8832a88f5c09a0909c9e1bb988c665c6284
Vladimir <Vovan_iva@mail.ru>
2020-04-16 18:41:32 +0300

    ппп

--- js/script.js
+++ js/script.js
@@ -1,43 +1,44 @@
 document.addEventListener('DOMContentLoaded', function(){
-  let images = document.querySelectorAll('.laptop__img'), // Все картинки в ноутбуке
-      placeTabs = document.querySelector('.laptop__tabs'), // Место для кнопок переключения картинок
-      active = 'is-active'; // Класс показывающий активную картинку и кнопку переключения
+  let laptops = document.querySelectorAll('.laptop');
   
-  // Функция переключения активного класс между элементами
-  const changeActiveClass = function(n, items) {
-   for (i = 0; i < items.length; i++) {
-     let item = items[i];
-     item.classList.remove(active);
+  for (l = 0; l < laptops.length; l++) {
+   let laptop = laptops[l],
+       images = laptop.querySelectorAll('.laptop__img'),
+       placeTabs = laptop.querySelector('.laptop__tabs'),
+       active = 'is-active';
+   
+   const changeActiveClass = function(n, elemets) {
+    for (e = 0; e < elemets.length; e++) {
+     let elemet = elemets[e];
+     elemet.classList.remove(active);
      
-     if (n == i) {
-      item.classList.add(active);
+     if (n == e) {
+      elemet.classList.add(active);
      };
     };
-  };
- 
-  // Создание кнопок переключения в зависимости от количества картинок
-  for (a = 0; a < images.length; a++) {
-   let el = document.createElement('li');
-   el.classList.add('laptop__tab');
-   placeTabs.append(el);
-  };
-  
-  let tabs = document.querySelectorAll('.laptop__tab'); // Все кнопки переключения
-  
-  // Функционал переключения картнок в зависимости какая кнопка нажата
-  for (t = 0; t < tabs.length; t++) {
-   let tab = tabs[t],
-       number = t;
+   };
    
-   // Добавление класса активации, первой кнопке и первой картинке
-   // Чтобы самому НЕ СТАВИТЬ эти классы
-   tabs[0].classList.add(active);
-   images[0].classList.add(active);
+   for (i = 0; i < images.length; i++) {
+    let img = images[i],
+        elTab = document.createElement('li');
+    elTab.classList.add('laptop__tab');
+    
+    if (i == 0) { img.classList.add(active); };
+    placeTabs.append(elTab);   
+   };
    
-   // Процесс, по нажатию на кнопку переключения происходит вызов функции переключения активного класса, как для самих кнопок, так и для картинок
-   tab.addEventListener('click', function(){
-    changeActiveClass(number,tabs);
-    changeActiveClass(number,images);
-   });
+   let tabs = laptop.querySelectorAll('.laptop__tab');
+   
+   for (t = 0; t < tabs.length; t++) {
+    let tab = tabs[t],
+        number = t;
+    
+    if (t == 0) { tab.classList.add(active); };
+    
+    tab.addEventListener('click', function(){
+     changeActiveClass(number,tabs);
+     changeActiveClass(number,images);
+    });
+   };
   };
  });
